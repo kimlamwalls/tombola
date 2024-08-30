0c01d04bad61e39b22bc4ff9ff71817d1097b96d
Kim Walls <79885327+kimlamwalls@users.noreply.github.com>
2024-08-31 00:36:00 +1000

    2d working

--- tombola.js
+++ tombola.js
@@ -1,34 +1,33 @@
-import * as Matter from 'matter-js';
 
-
+// Module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
-    Composites = Matter.Composites,
-    Composite = Matter.Composite,
-    Bodies = Matter.Bodies;
-    
-var tombola = new Engine.Runner(document.querySelector('.tombola__container'));
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+// Create an engine
 var engine = Engine.create();
-// create a renderer
+
+// Create a renderer
 var render = Render.create({
     element: document.body,
     engine: engine
 });
 
-// create two boxes and a ground
+// Create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-// add all of the bodies to the world
+// Add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground]);
 
-// run the renderer
+// Run the renderer
 Render.run(render);
 
-// create runner
+// Create runner
 var runner = Runner.create();
 
-// run the engine
+// Run the engine
 Runner.run(runner, engine);
